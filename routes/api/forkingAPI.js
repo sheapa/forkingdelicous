9fb0490a051da25be80b11bbf89344bd98fc869b
Ryan Antonelli <ryan.a.antonelli@gmail.com>
2020-05-25 17:34:30 -0700

    added recipe controller and routes to existing files

--- routes/api/forkingAPI.js
+++ routes/api/forkingAPI.js
@@ -5,10 +5,19 @@ const forkingController = require("../../controllers/forkingController");
 // router.route("/").get(forkingController.findAll).post(forkingController.create);
 
 // Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(forkingController.findById)
-  .put(forkingController.update)
-  .delete(forkingController.remove);
+router.route("/:id")
+    .get(forkingController.findById)
+    .put(forkingController.update)
+    .delete(forkingController.remove)
+  
+    .get(forkingController.findByRecipeId)
+    .put(forkingController.updateRecipe)
+    .delete(forkingController.removeRecipe)
+
+router.route("/")
+    .post(forkingController.create)
+
+    .get(forkingController.findAllRecipes)
+    .post(forkingController.createRecipe)
 
 module.exports = router;
