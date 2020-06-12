57f87c908a7530bdda40d3afbed053b1b7f7e3e7
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-06-12 14:29:14 -0700

    commit needed for merge

--- models/recipe.js
+++ models/recipe.js
@@ -1,44 +1,45 @@
-const mongoose = require("mongoose");
 // import mongoolia from 'mongoolia';
+const mongoolia = require('mongoolia').default;
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const RecipeSchema = new Schema({
-  // title: {type: Schema.Types.String, algoliaIndex: true},
-  title: [Schema.Types.String],
-  description: Schema.Types.String,
-  instruction: [Schema.Types.String],
+  title: {type: Schema.Types.String, algoliaIndex: true},
+  // title: [Schema.Types.String],
+  description: {type: Schema.Types.String, algoliaIndex: true},
+  instruction: [{type: Schema.Types.String, algoliaIndex: true}],
   ingredients: [
     {
-      name: Schema.Types.String,
-      quantity: Schema.Types.Number,
-      unit: Schema.Types.String,
+      name: {type: Schema.Types.String, algoliaIndex: true},
+      quantity: {type: Schema.Types.Number, algoliaIndex: true},
+      unit: {type: Schema.Types.String, algoliaIndex: true},
     },
   ],
-  tips: Schema.Types.String,
-  yeild: Schema.Types.String,
-  categories: [Schema.Types.String],
-  feedback: [Schema.Types.String],
-  activeTime: Schema.Types.String,
-  inActiveTime: Schema.Types.String,
-  addons: { type: Schema.Types.ObjectId, ref: "Recipe" },
-  forkedFrom: { type: Schema.Types.ObjectId, ref: "User" },
-  images: [Schema.Types.String],
-  pubDate: {
-    type: Schema.Types.Date,
-    default: Date.now,
-  },
-  likes: Schema.Types.Number,
-  saves: Schema.Types.Number,
-  forks: Schema.Types.Number,
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  tips: {type: Schema.Types.String, algoliaIndex: true},
+  yeild: {type: Schema.Types.String, algoliaIndex: true},
+  categories: [{type: Schema.Types.String, algoliaIndex: true}],
+  feedback: [{type: Schema.Types.String, algoliaIndex: true}],
+  activeTime: {type: Schema.Types.String, algoliaIndex: true},
+  inActiveTime: {type: Schema.Types.String, algoliaIndex: true},
+  addons: { type: Schema.Types.ObjectId, ref: "Recipe", algoliaIndex: true },
+  forkedFrom: { type: Schema.Types.ObjectId, ref: "User", algoliaIndex: true },
+  images: [{type: Schema.Types.String, algoliaIndex: true}],
+  pubDate: [
+    {type: Schema.Types.String, algoliaIndex: true},
+    {default: Date.now, algoliaIndex: true}
+  ],
+  likes: {type: Schema.Types.Number, algoliaIndex: true},
+  saves: {type: Schema.Types.Number, algoliaIndex: true},
+  forks: {type: Schema.Types.Number, algoliaIndex: true},
+  author: { type: Schema.Types.ObjectId, ref: "User", algoliaIndex: true },
 });
 
-// RecipeSchema.plugin(mongoolia, {
-//   appId: 'ZCHNRWDF6B',
-//   apiKey: 'ee98d1240ef700b8e63342844d760803',
-//   indexName: 'recipeDB'
-// })
+RecipeSchema.plugin(mongoolia, {
+  appId: 'ZCHNRWDF6B',
+  apiKey: 'af3f80f79bfd2b7d0e7832aba6f0a582',
+  indexName: 'recipeDB'
+})
 
 const Recipe = mongoose.model("Recipe", RecipeSchema);
 
